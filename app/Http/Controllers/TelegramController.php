8cd7fc90b41ab0025860951a0efc1538e855b81e
dk1gdo <d.garieff@yandex.ru>
2023-05-31 00:03:19 +0500

    test tg first-10

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -15,10 +15,23 @@ class TelegramController extends Controller
      * @return \Illuminate\Http\Response
      */
 
+    private string $token = "6158072722:AAEorSAQWz_qgYiKrnrln44ChWSesZgZ3zo";
     public function __invoke(Request $request)
     {
-        $service = new TelegramService();
-        $service->sendMessage($request->getContent());
-        return 2;
+        $ch = curl_init();
+        $ch_post = [
+            CURLOPT_URL => 'https://api.telegram.org/bot' . $this->token . '/sendMessage',
+            CURLOPT_POST => TRUE,
+            CURLOPT_RETURNTRANSFER => TRUE,
+            CURLOPT_TIMEOUT => 10,
+            CURLOPT_POSTFIELDS => [
+                'parse_mode' => 'HTML',
+                'text' => $request->getContent(),
+            ]
+        ];
+
+        curl_setopt_array($ch, $ch_post);
+        curl_exec($ch);
+        //return 2;
     }
 }
