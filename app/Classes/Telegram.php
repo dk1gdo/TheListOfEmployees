b0f498d60d19478c20cd6083997747fce03a3e26
dk1gdo <d.garieff@yandex.ru>
2023-05-31 11:58:57 +0500

    test tg first-25

--- app/Classes/Telegram.php
+++ app/Classes/Telegram.php
@@ -9,7 +9,7 @@ class Telegram
     protected string $token = "6158072722:AAEorSAQWz_qgYiKrnrln44ChWSesZgZ3zo";
 
     public function sendMessage($chat_id, $message) {
-        Http::post('https://api.telegram.org/bot' . $this->token . '/sendMessage', [
+        return Http::post('https://api.telegram.org/bot' . $this->token . '/sendMessage', [
             'chat_id' => $chat_id,
             'parse_mode' => 'HTML',
             'text' => 'You message => [' . $message . "]",
